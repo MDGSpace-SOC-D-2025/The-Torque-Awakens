bd93e4755c2f5c141c2d1c6db2794658b809e1c7
m1rabil1z <abhirupmahato488@gmail.com>
2026-01-28 08:54:31 +0530

    feat: Support icon changed

--- scripts/TrussConfig.gd
+++ scripts/TrussConfig.gd
@@ -1,5 +1,4 @@
 extends Resource
-
 class_name TrussConfig
 
 @export var snap_radius: float = 25.0
@@ -7,3 +6,10 @@ class_name TrussConfig
 @export var line_thickness: float = 5.0
 @export var line_color: Color = Color.WHITE
 @export var border_color: Color = Color.BLACK
+
+@export var pin_texture: Texture2D
+@export var roller_texture: Texture2D
+@export var icon_size: Vector2 = Vector2(64, 64)
+
+@export var pin_hole_offset: Vector2 = Vector2.ZERO
+@export var roller_hole_offset: Vector2 = Vector2.ZERO
